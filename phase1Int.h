e7c9ffe055dbe7f662eacee049662a9823d9b32e
John H. Hartman <john.hartman@akamai.com>
2019-10-02 11:07:07 -0700

    Force check of return value.

--- phase1Int.h
+++ phase1Int.h
@@ -12,17 +12,18 @@
 // Phase 1a
 
 void    P1ContextInit(void);
-int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid);
-int     P1ContextSwitch(int cid);
-int     P1ContextFree(int cid);
-int     P1DisableInterrupts(void);
+int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid) CHECKRETURN; 
+int     P1ContextCreate(void (*func)(void *), void *arg, int stacksize, int *cid) CHECKRETURN; 
+int     P1ContextSwitch(int cid) CHECKRETURN;
+int     P1ContextFree(int cid) CHECKRETURN;
+int     P1DisableInterrupts(void) CHECKRETURN;
 void    P1EnableInterrupts(void);
 
 // Phase 1b
 
 void    P1ProcInit(void);
-int     P1GetChildStatus(int tag, int *pid, int *status);
-int     P1SetState(int pid, P1_State state, int sid);
+int     P1GetChildStatus(int tag, int *pid, int *status) CHECKRETURN;
+int     P1SetState(int pid, P1_State state, int sid) CHECKRETURN;
 void    P1Dispatch(int rotate);
 
 // Phase 1c
